0418b406016b68cf108ad2db9d25ae01af2e4e63
svenhominal <sven.hominal@epfl.ch>
2024-12-08 22:08:39 +0100

    streamlit app

---
+++ streamlit_website.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Welcome to My Streamlit App")
+st.write("This is a simple Streamlit app integrated with a Jekyll website.")  
+
+
+# To run : streamlit run app.py in terminal  
